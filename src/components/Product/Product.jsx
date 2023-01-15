c9107be61c7143a1a6e88920ccd147146a7514f0
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-15 22:10:37 +0600

    ema jhon

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,11 +3,14 @@ import "./Product.css";
 
 export default function Product({ name, img, seller, price, rating }) {
   return (
+
     <div className="product">
       <img src={img} alt="" />
-      <h2>{name}</h2>
-      <p>Price: {price}</p>
+      <div className="product-discription">
+      <p className="product-name">{name}</p>
+      <p className="product-price">Price: {price}</p>
       <p>By: {seller}</p>
+      </div>
     </div>
   );
 }
