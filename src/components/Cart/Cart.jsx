7cddbde6e43c08d7bc1de5d45cb24058653b5d95
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-23 23:57:46 +0600

    data show in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,16 +2,31 @@ import React from 'react'
 import './Cart.css'
 
 export default function Cart({cart}) {
-  console.log(cart);
-  
+
+
+ /*-------------reduce method----------------  
+  const total = cart.reduce((total, previousTotal) => total + previousTotal.price, 0);
+  const shipping = cart.reduce((shipping, previousShipping) => shipping + previousShipping.shipping, 0); 
+  --------------------------------------*/
+// for of mathood
+  let total = 0;
+  let shipping = 0;
+  for (const product of cart) {
+    total += product.price;
+    shipping += product.shipping;
+  }
+
+  const tax = parseInt((total * 0.1).toFixed(2));
+  const grandTotal = total + shipping + tax;
+
   return (
     <div className='cart'>
         <h1>Order Summary</h1>
         <h3>Items Ordered: {cart.length}</h3>
-        <p>Total Price : </p>
-        <p>Total Shipping : </p>
-        <p>Tax :</p>
-        <h4>Grand Total :</h4>
+        <p>Total Price : {total}</p>
+        <p>Total Shipping :{shipping} </p>
+        <p>Tax : {tax}</p>
+        <h4>Grand Total : {grandTotal}</h4>
     </div>
   )
 }
